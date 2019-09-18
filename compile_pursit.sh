8ceaae64232a8c64f2b004e88f9834574ef77975
FusionResearch <moerafaat@aucegypt.edu>
2019-09-19 01:12:49 +0200

    feature compile c++ in partition dir

---
+++ compile_pursit.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+export CONDAENV=/home/fusionresearch/anaconda3/envs/superpoint_oxford
+cd partition/ply_c
+cmake . -DPYTHON_LIBRARY=$CONDAENV/lib/libpython3.6m.so -DPYTHON_INCLUDE_DIR=$CONDAENV/include/python3.6m -DBOOST_INCLUDEDIR=$CONDAENV/include -DEIGEN3_INCLUDE_DIR=$CONDAENV/include/eigen3
+make
+cd ..
+cd cut-pursuit
+mkdir build
+cd build
+cmake .. -DPYTHON_LIBRARY=$CONDAENV/lib/libpython3.6m.so -DPYTHON_INCLUDE_DIR=$CONDAENV/include/python3.6m -DBOOST_INCLUDEDIR=$CONDAENV/include -DEIGEN3_INCLUDE_DIR=$CONDAENV/include/eigen3
+make
